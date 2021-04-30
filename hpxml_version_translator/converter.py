86f508b60a49ae74bab3b1971ea3f252bdfd2433
bpark1327 <benjamin.park@nrel.gov>
2021-04-30 07:42:11 -0600

    first pass

--- hpxml_version_translator/converter.py
+++ hpxml_version_translator/converter.py
@@ -675,8 +675,19 @@ def convert_hpxml2_to_3(hpxml2_file, hpxml3_file):
     # TODO: Standardize Locations
     # https://github.com/hpxmlwg/hpxml/pull/156
 
-    # TODO: Lighting Fraction Improvements
+    # Lighting Fraction Improvements
     # https://github.com/hpxmlwg/hpxml/pull/165
+
+    total_num_units_in_location = defaultdict(int)
+    for i, ltggrp in enumerate(root.xpath('//h:Lighting/h:LightingGroup', **xpkw)):
+        total_num_units_in_location[str(ltggrp.Location)] += ltggrp.NumberofUnits
+
+    for i, ltggrp in enumerate(root.xpath('//h:Lighting/h:LightingGroup', **xpkw)):
+        ltg = ltggrp.getparent()
+        frac_of_units = round(ltggrp.NumberofUnits / total_num_units_in_location[str(ltggrp.Location)], 3)
+        ltggrp.NumberofUnits.addnext(E.FractionofUnitsInLocation(frac_of_units))
+        if hasattr(ltg, 'LightingFractions'):
+            ltg.remove(ltg.LightingFractions)
 
     # TODO: Deprecated items
     # https://github.com/hpxmlwg/hpxml/pull/167
